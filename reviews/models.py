bd16ee3139fa0fccb79819a9eaa7510519971f55
eayub006-source <eayub006@gmail.com>
2026-07-06 04:48:29 +0500

    Initial Django project setup with Review model and database

---
+++ reviews/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Review(models.Model):
+    TYPE_CHOICES = [
+        ('book', 'Book'),
+        ('movie', 'Movie'),
+    ]
+
+    title = models.CharField(max_length=200)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    rating = models.IntegerField()
+    review = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.type})"
+    

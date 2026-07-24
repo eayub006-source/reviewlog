31d2e6932e4fde32a6123e84fa646458a78a85fa
eayub006-source <eayub006@gmail.com>
2026-07-24 18:00:58 +0500

    feat: implement review ownership and protected review endpoints

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,21 @@
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Review(models.Model):
     TYPE_CHOICES = [
-        ('book', 'Book'),
-        ('movie', 'Movie'),
+        ("book", "Book"),
+        ("movie", "Movie"),
     ]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        null=True,
+        blank=True,
+    )
 
     title = models.CharField(max_length=200)
     type = models.CharField(max_length=10, choices=TYPE_CHOICES)
@@ -15,4 +25,3 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.type})"
-    
--- reviews/views/review_views.py
+++ reviews/views/review_views.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from ..models import Review
 from ..serializers import ReviewSerializer
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all().order_by("-date")
+    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Review.objects.filter(user=self.request.user).order_by("-date")
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
